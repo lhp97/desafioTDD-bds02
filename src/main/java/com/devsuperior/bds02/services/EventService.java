f66496ed5b92a3c634d048de376462d7b3d1f5c7
lhp97 <lhdprado@gmail.com>
2022-10-29 06:19:06 -0300

    tests completeds

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,2 +1,34 @@
-package com.devsuperior.bds02.services;public class EventService {
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Transactional
+    public EventDTO updateEvent(Long id, EventDTO eventDTO) {
+        try {
+            Event event = eventRepository.getOne(id);
+            event.setName(eventDTO.getName());
+            event.setCity(new City(eventDTO.getCityId(), null));
+            event.setDate(eventDTO.getDate());
+            event.setUrl(eventDTO.getUrl());
+            event = eventRepository.save(event);
+            return new EventDTO(event);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found: " + id);
+        }
+    }
 }
